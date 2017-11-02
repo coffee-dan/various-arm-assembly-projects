664a0e038774f65147bf8f8a3e797099debb0f91
coffee-dan <31706913+coffee-dan@users.noreply.github.com>
2017-11-01 20:27:24 -0500

    New mod function to be tested.

--- dgr2815_p2.s
+++ dgr2815_p2.s
@@ -12,7 +12,9 @@ writeloop:
     ADD R2, R1, R2          @ R2 now has the element address
     PUSH {R0}               @ backup iterator before procedure call
     PUSH {R2}               @ backup element address before procedure call
+	PUSH {R1}				@ backup R1 (a address) for _mod_unsigned call
     BL _getrand             @ get a random number
+	POP {R1} 				@ restore R1
     POP {R2}                @ restore element address
     STR R0, [R2]            @ write the address of a[i] to a[i]
     POP {R0}                @ restore iterator
@@ -66,8 +68,27 @@ _seedrand:
     
 _getrand:
     PUSH {LR}               @ backup return address
-    BL rand                 @ get a random number
+	BL rand                 @ get a random number
+	MOV R1, R0
+	MOV R2, #1000
+    BL  _mod_unsigned   	@ compute the remainder of R1 / R2
     POP {PC}                @ return 
+	
+_mod_unsigned:
+    cmp R2, R1          	@ check to see if R1 >= R2
+    MOVHS R0, R1        	@ swap R1 and R2 if R2 > R1
+    MOVHS R1, R2        	@ swap R1 and R2 if R2 > R1
+    MOVHS R2, R0     	   	@ swap R1 and R2 if R2 > R1
+    MOV R0, #0       	  	@ initialize return value
+    B _modloopcheck   	 	@ check to see if
+    _modloop:
+        ADD R0, R0, #1  	@ increment R0
+        SUB R1, R1, R2  	@ subtract R2 from R1
+    _modloopcheck:
+        CMP R1, R2      	@ check for loop termination
+        BHS _modloop   	 	@ continue loop if R1 >= R2
+    MOV R0, R1          	@ move remainder to R0
+    MOV PC, LR          	@ return
    
 .data
 
