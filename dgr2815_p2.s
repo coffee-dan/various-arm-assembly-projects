5c949b06c60e99c4ef3c1a28470bca34870e5dcb
coffee-dan <31706913+coffee-dan@users.noreply.github.com>
2017-11-01 23:01:46 -0500

    Small change to min_check logic

--- dgr2815_p2.s
+++ dgr2815_p2.s
@@ -4,7 +4,7 @@
 main:
     BL _seedrand            @ seed random number generator with current time
     MOV R0, #0              @ initialze index variable
-	MOV R3, #1000			@ initialize max value for _min_check
+	MOV R5, #1000			@ initialize max value for _min_check
 	MOV R4, #0				@ initialize min value for _max_check
 writeloop:
     CMP R0, #10             @ check to see if we are done iterating
@@ -101,13 +101,8 @@ _mod_unsigned:
     MOV PC, LR          	@ return
 	
 _min_check:
-	CMP R2, R3				@ check to see if R2 < R3
-	MOVLS R3, R2			@ new min value is R2
-	PUSH {R1}
-	LDR R0, =debug_str
-	MOV R1, R3
-	BL printf
-	POP {R1}
+	CMP R2, R5				@ check to see if R2 < R3
+	MOVLS R5, R2			@ new min value is R2
 	MOV PC, LR				@ return
 	
 _max_check:
