7204d79b858b7526b3c64407844e230d1716cc44
coffee-dan <31706913+coffee-dan@users.noreply.github.com>
2017-11-16 23:41:45 -0600

    Swapping registers used

--- dgr2815_p4.s
+++ dgr2815_p4.s
@@ -16,20 +16,22 @@ _loop:
 	VMOV R1, R2, D4
 	BL _first_print
 	
+	MOV R0, #0
 	MOV R1, #0
 	MOV R2, #0
 	
 	VCVT.F32.S32 S1, S1     @ convert unsigned bit representation to single float
-	VCVT.F64.F32 D4, S1
-	VMOV R1, R2, D4
+	VCVT.F64.F32 D5, S1
+	VMOV R1, R2, D5
 	BL _second_print
 	
+	MOV R0, #0
 	MOV R1, #0
 	MOV R2, #0
 	
 	VDIV.F32 S2, S0, S1     @ compute S2 = S0 / S1
-	VCVT.F64.F32 D4, S2     @ covert the result to double precision for printing
-	VMOV R1, R2, D4         @ split the double VFP register into two ARM registers
+	VCVT.F64.F32 D6, S2     @ covert the result to double precision for printing
+	VMOV R1, R2, D6         @ split the double VFP register into two ARM registers
 	BL _third_print
 	
 	B _loop					@ infinite loop
