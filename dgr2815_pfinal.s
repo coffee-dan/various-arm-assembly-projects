ff78aabcf91bb48cee8361f7e0214d7f14c1f241
coffee-dan <31706913+coffee-dan@users.noreply.github.com>
2017-12-06 18:39:25 -0600

    Attempt 2

--- dgr2815_pfinal.s
+++ dgr2815_pfinal.s
@@ -52,7 +52,7 @@ readloop:
 readdone:
 	BL _prompt				@ branch to prompt procedure with return
 	BL _scanf				@ branch to scanf procedure with return
-	MOV R3, R0				@ move scanf value to R3
+	MOV R5, R0				@ move scanf value to R5
 	MOV R0, #0              @ initialze index variable
 searchloop:
 	CMP R0, #10             @ check to see if we are done iterating
@@ -61,9 +61,9 @@ searchloop:
     LSL R2, R0, #2          @ multiply index*4 to get array offset
     ADD R2, R1, R2          @ R2 now has the element address
     LDR R1, [R2]            @ read the array at address 
-	PUSH {R3}
-	CMP R1, R3
-	POP {R3}
+	PUSH {R5}				@!CMP is not corrupting R5
+	CMP R1, R5
+	POP {R5}
     PUSHEQ {R0}               @ backup register before printf
     PUSHEQ {R1}               @ backup register before printf
     PUSHEQ {R2}               @ backup register before printf
